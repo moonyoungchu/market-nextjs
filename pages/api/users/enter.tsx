21d537f299ccf7279a09626e32034b812f3c8968
Moonyoung Chu <cnansdud11@hanmail.net>
2023-06-29 23:10:46 +0900

    feat: #9 토큰추가, 문자보내기, 이메일보내기 기능 추가
    - 문자 : twilio
    - 이메일 : Mailtrap, nodemailer

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,63 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
-import withHandler from "@libs/server/withHandler";
+import twilio from "twilio";
+import sendEmail from "@libs/server/sendEmail";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(">>>body", req.body);
-  return res.status(200).end();
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { phone, email } = req.body;
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+
+  if (!user) {
+    return res.status(400).json({ ok: false });
+  }
+
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
+    },
+  });
+
+  console.log(">>>payload", payload);
+  console.log(">>>user", user);
+  console.log(">>>token", token);
+
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE_NUMBER!,
+      body: `Your login token is ${payload}.`,
+    });
+    console.log("문자테스트", message);
+  } else if (email) {
+    const emailSending = await sendEmail({
+      email: email,
+      subject: "Login token",
+      message: `Hi! Your login token is ${payload}.`,
+    });
+  }
+
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
